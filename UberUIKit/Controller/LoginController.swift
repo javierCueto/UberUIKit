6fb193a77be190e34017bfb3ca0e590c4d8e8d0e
javierCueto <javelie@outlook.com>
2020-11-09 19:26:21 -0700

    uber title just code

--- UberUIKit/Controller/LoginController.swift
+++ UberUIKit/Controller/LoginController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
+    // MARK: -  properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    // MARK: -  Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 40).isActive = true
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
