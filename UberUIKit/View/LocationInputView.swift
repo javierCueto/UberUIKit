bf9500f02f016e13ef3da39bb569029ab59432d1
javierCueto <javelie@outlook.com>
2020-11-11 17:38:14 -0700

    add inputs and mini views in location view

--- UberUIKit/View/LocationInputView.swift
+++ UberUIKit/View/LocationInputView.swift
@@ -22,6 +22,60 @@ class LocationInputView: UIView {
         return button
     }()
     
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Javier Cueto"
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let startLocationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let linkingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    private let destinationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    
+    private lazy var startigLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Current location"
+        tf.backgroundColor = .systemGroupedBackground
+        tf.isEnabled = false
+        tf.font = UIFont.systemFont(ofSize: 14)
+        let paddinView = UIView()
+        paddinView.setDimentions(height: 30, width: 8)
+        tf.leftView = paddinView
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    private lazy var destinationLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter destination"
+        tf.backgroundColor = .lightGray
+        tf.returnKeyType = .search
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddinView = UIView()
+        paddinView.setDimentions(height: 30, width: 8)
+        tf.leftView = paddinView
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
     // MARK: -  lifecyle
 
     override init(frame: CGRect) {
@@ -30,6 +84,33 @@ class LocationInputView: UIView {
         addShadow()
         backgroundColor = .white
         backButton.anchor(top: topAnchor, left: leftAnchor, paddingTop: 44, paddingLeft: 12, width: 24, height: 24)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: backButton)
+        titleLabel.centerX(inView: self)
+        
+        addSubview(startigLocationTextField)
+        startigLocationTextField.anchor(top: backButton.bottomAnchor, left: leftAnchor, right: rightAnchor,
+                                        paddingTop: 4, paddingLeft: 40, paddingRight: 40,height: 30)
+        addSubview(destinationLocationTextField)
+        destinationLocationTextField.anchor(top: startigLocationTextField.bottomAnchor, left: leftAnchor, right: rightAnchor,
+                                        paddingTop: 12, paddingLeft: 40, paddingRight: 40,height: 30)
+        
+        
+        addSubview(startLocationIndicatorView)
+        startLocationIndicatorView.centerY(inView: startigLocationTextField,leftAnchor: leftAnchor,paddingLeft: 20)
+        startLocationIndicatorView.setDimentions(height: 6, width: 6)
+        startLocationIndicatorView.layer.cornerRadius = 6 / 2
+        
+        addSubview(destinationIndicatorView)
+        destinationIndicatorView.centerY(inView: destinationLocationTextField,leftAnchor: leftAnchor,paddingLeft: 20)
+        destinationIndicatorView.setDimentions(height: 6, width: 6)
+        
+        addSubview(linkingView)
+        linkingView.centerX(inView: startLocationIndicatorView)
+        linkingView.anchor(top: startLocationIndicatorView.bottomAnchor,
+                           bottom: destinationIndicatorView.topAnchor,
+                           paddingTop: 4, paddingBottom: 4, width: 0.5)
         
     }
     
